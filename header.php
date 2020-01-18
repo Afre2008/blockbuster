2bc147fb474e5a06ab9e61cf5c1e717a2814e432
Afre2008 <afre2008@gmail.com>
2020-01-18 09:52:53 -0300

    first commit[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[Cinicial

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php require_once('functions.php'); ?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Blockbuster</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="/">
+                <img src="img/Blockbuster_logo.png" alt="Blockbuster" style="width: 253px;">
+            </a>
+
+    </header>
